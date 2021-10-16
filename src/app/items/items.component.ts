5c2ecbb53fc060e5bbbca21dfa0dac4c4453e47b
Bob Rinehart <rinebob111185@gmail.com>
2021-10-16 13:58:48 -0700

    Created Items Component

---
+++ src/app/items/items.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gf-items',
+  templateUrl: './items.component.html',
+  styleUrls: ['./items.component.sass']
+})
+export class ItemsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
